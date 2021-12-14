47b5e79639b4d93bb62a0e42b489437fdd5362de
ll <ll@gmail.com>
2021-12-14 16:29:25 +0100

    Test scriptable
    Test ScriptableObject

---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Danced-Out/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public GameObject EnemyMove;
+    //public State EnemyState;
+}
